d29679f7463cb0ebb8a19faa72230fe41caa74de
Shawn59 <xxx_respekt.ru@mail.ru>
2020-01-22 22:01:48 +0500

    #1.0.0.0 - разбор полётов по webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 "use strict";
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     entry: './app/index.js', // главный файл
@@ -8,7 +9,30 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
 
-   /* devServer: {
-        stats: "error-only",
-    }*/
+    module: {
+        rules: [
+            { // Для сборки js-jsx файлов
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                },
+            },
+            {// Для сборки css файлов
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ],
+    }
+
+    /* devServer: {
+         stats: "error-only",
+     }*/
 };
+
+/*
+babel-core: Преобразует код ES6 в ES5.
+babel-loader: Помощник Webpack для транспайлинга кода, задает пресеты.
+babel-preset-env: Пресет, который помогает babel конвертировать код ES6, ES7 и ES8 в код ES5.
+babel-preset-react: Пресет, преобразующий JSX в JavaScript.
+* */
